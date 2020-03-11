34adacf7fd655642c949a66275a17c11562e0e05
DeadneM <deadnem@gmail.com>
2020-03-11 21:50:52 +0100

    Crash fix when using bipod or m203 when bleedingout

--- lua/PlayerBleedOut.lua
+++ lua/PlayerBleedOut.lua
@@ -1,7 +1,7 @@
 function PlayerBleedOut:_update_check_actions(t, dt)
-	local input = self:_get_input()
+	local input = self:_get_input(t, dt)
 	self._unit:camera():set_shaker_parameter("headbob", "amplitude", 0)
-	self:_update_throw_grenade_timers(t, input)
+	self:_update_throw_projectile_timers(t, input)
 	self:_update_reload_timers(t, dt, input)
 	self:_update_melee_timers(t, input)
 	self:_update_equip_weapon_timers(t, input)
@@ -11,24 +11,28 @@ function PlayerBleedOut:_update_check_actions(t, dt)
 		self._unit:base():set_stats_screen_visible(false)
 	end
 	self:_update_foley(t, input)
-	local new_action
+	local new_action = nil
 	new_action = new_action or self:_check_action_weapon_gadget(t, input)
 	new_action = new_action or self:_check_action_weapon_firemode(t, input)
-	new_action = new_action or PlayerStandard._check_action_melee(self, t, input)
+	new_action = new_action or self:_check_action_melee(t, input)
 	new_action = new_action or self:_check_action_reload(t, input)
 	new_action = new_action or self:_check_change_weapon(t, input)
 	if not new_action then
 		new_action = self:_check_action_primary_attack(t, input)
-		self._shooting = new_action
+		if not _G.IS_VR then
+			self._shooting = new_action
+		end
 	end
-	new_action = new_action or self:_check_action_throw_grenade(t, input)
+	new_action = new_action or self:_check_action_throw_projectile(t, input)
 	new_action = new_action or self:_check_action_equip(t, input)
 	new_action = new_action or self:_check_action_interact(t, input)
 	new_action = new_action or self:_check_action_steelsight(t, input)
-	PlayerCarry._check_use_item(self, t, input)
+	new_action = new_action or self:_check_action_deploy_underbarrel(t, input)
+	self:_check_action_night_vision(t, input)
+	self:_check_use_item(t, input)
 end
 
---FROM EXTRA
+--Not sure how those below lines are usefull
 Hooks:PostHook( PlayerBleedOut , "update" , "MeleeOverhaulExtrasPlayerBleedOutPostUpdate" , function( self , t , dt )
 
 	local input = self:_get_input()
